5f473091f1a8366fc739e0cb0f303b0e623adaf8
T0nci <138928016+T0nci@users.noreply.github.com>
2024-11-23 16:35:02 +0100

    Add custom error handling

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const CustomError = require("./utils/CustomError");
 
 const app = express();
 
@@ -12,6 +13,22 @@ app.use(
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// if no route matched then this is a 404
+app.use((req, res, next) => {
+  next(new CustomError(404, "Not Found"));
+});
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
+  if (!err.statusCode) {
+    console.error(err);
+    err.statusCode = 500;
+    err.message = "Internal Server Error";
+  }
+  res
+    .status(err.statusCode)
+    .json({ error: `${err.statusCode}: ${err.message}` });
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Blog API listening on port ${PORT}!`));
